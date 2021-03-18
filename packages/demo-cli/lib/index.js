ffb2b6d00257486d350dd6485f7bb24d7f3348bc
lijunfeng <junfeng.li@hand-china.com>
2021-03-18 17:41:15 +0800

    commander

--- packages/demo-cli/lib/index.js
+++ packages/demo-cli/lib/index.js
@@ -1,1 +1,27 @@
-console.log('------------')
+const commander = require('commander')
+const program = commander;
+
+program
+  .version(require('../package.json').version, '-v, --version')
+  .usage('<command> [options]');
+
+program
+  .command('new <name>')
+  .alias('n')
+  .description('Generate Hzero application')
+  .option(
+    '-t, --template <template-name>',
+    'choose template'
+  )
+  .option(
+    '-ts, --template-ss <template-name>',
+    'choose template',
+    'test'
+  )
+  .action((name,options,command) => {
+    console.log(name)
+    console.log(options)
+    console.log(command.opts())
+  })
+
+commander.parse(process.argv);
