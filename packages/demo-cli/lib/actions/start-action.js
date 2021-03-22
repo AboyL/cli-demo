6af9a8c1280287379e1af55007e585d19d64f617
lijunfeng <junfeng.li@hand-china.com>
2021-03-22 10:55:40 +0800

    start 更新

--- packages/demo-cli/lib/actions/start-action.js
+++ packages/demo-cli/lib/actions/start-action.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const open = require('open');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./configs/webpack.config')
 const port = 8333
@@ -6,8 +7,14 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 module.exports = () => {
   const compiler = webpack(webpackConfig);
-  const devServer = new WebpackDevServer(compiler);
+
+  const devServer = new WebpackDevServer(compiler, {
+    quiet: true,// 不要webpack编译的时候打印的日志 https://webpack.docschina.org/configuration/stats/
+  });
+
   devServer.listen(port, HOST, () => {
-    console.log('启动成功')
+    const url = `http://localhost:${port}`
+    console.log('服务启动于', url)
+    open(url)
   })
 }
